16433c163300ffdb429f4626d028aa3b4f6143d5
dmojarrot <a01638460@itesm.mx>
2022-04-05 20:19:15 -0500

    Update DarkMode.jsx

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -12,14 +12,14 @@ function DarkMode() {
       checked={theme === 'dark' ? true : false}
       onChange={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
       className={classNames(
-        theme === 'dark' ? 'bg-light-primary' : 'bg-dark-bg',
+        theme === 'dark' ? 'bg-light-primary' : 'bg-light-bg',
         'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
       )}>
       <span className='sr-only'>Use setting</span>
       <span
         className={classNames(
           theme === 'dark' ? 'translate-x-5' : 'translate-x-0',
-          'pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+          'pointer-events-none relative inline-block h-5 w-5 rounded-full bg-dark-bg shadow transform ring-0 transition ease-in-out duration-200'
         )}>
         <span
           className={classNames(
