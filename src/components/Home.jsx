1bc6b2674c0a9ad3b808ab8421f988b3861f8032
dmojarrot <a01638460@itesm.mx>
2022-04-05 21:09:04 -0500

    Refactored

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='pl-20 pt-20 text-4xl font-bold'>
+      <p>Hi,</p>
+      <div className='flex'>
+        <p>I'm</p>
+        &nbsp;
+        <p className='text-light-primary dark:text-dark-primary'>Alex</p>
+      </div>
+      <p>Senior Javascript Developer.</p>
+    </div>
+  )
+}
+
+export default Home
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,10 @@
 import Header from '../components/Header'
+import Home from '../components/Home'
 export default function index() {
   return (
     <div className='bg-light-bg dark:bg-dark-bg h-screen'>
       <Header />
-      <div className='pl-20 pt-20 text-4xl font-bold'>
-        <p>Hi,</p>
-        <div className='flex'>
-          <p>I'm</p>
-          &nbsp;
-          <p className='text-light-primary'>Alex</p>
-        </div>
-        <p>Senior Javascript Developer.</p>
-      </div>
+      <Home />
     </div>
   )
 }
